37bf6f2810629a48236b806145577caec5ac1b81
tyler51217 <tyler-999@hotmail.com>
2026-07-28 23:28:05 -0400

    redid input system and added attack feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,21 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D playerRigidbody;
-    public InputAction playerMovement;
     private Vector2 moveDirection;
-    public int moveForce = 50;
+    public int moveForce = 10;
+    private bool attacking = false;
+    public InputActionAsset inputActions;
+    public Camera mainCam;
+    public LineRenderer lr;
 
+    private InputAction moveAction;
+    private InputAction attackAction;
 
+    private void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        attackAction = InputSystem.actions.FindAction("Attack");
+    }
     void Start()
     {
         
@@ -16,23 +26,45 @@ public class PlayerController : MonoBehaviour
 
     private void OnEnable()
     {
-        playerMovement.Enable();
+        inputActions.FindActionMap("Player").Enable();
     }
     private void OnDisable()
     {
-        playerMovement.Disable();
+        inputActions.FindActionMap("Player").Disable();
     }
 
     
+
     void Update()
     {
-        moveDirection = playerMovement.ReadValue<Vector2>();
+        moveDirection = moveAction.ReadValue<Vector2>();
+
+        if (attackAction.ReadValue<float>() == 1)
+            attacking = true;
+        else
+            attacking = false;
     }
     private void FixedUpdate()
     {
-        playerRigidbody.AddForceX(moveDirection.x * moveForce);
-        playerRigidbody.AddForceY(moveDirection.y * moveForce);
+        playerRigidbody.AddForceX(moveDirection.x * moveForce, ForceMode2D.Impulse);
+        playerRigidbody.AddForceY(moveDirection.y * moveForce, ForceMode2D.Impulse);
         //add coroutine here so only move once per second
+
+        if (attacking)
+        {
+            //draw line from player to mouse
+
+            lr.enabled = true;
+            lr.startWidth = 0.3f;
+            lr.endWidth = 0.3f;
+            lr.positionCount = 2;
+            lr.startColor = Color.white;
+            lr.endColor = Color.white;
+            lr.SetPosition(0, playerRigidbody.position);
+            lr.SetPosition(1, mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
+
+            
+        }
     }
 
 }
